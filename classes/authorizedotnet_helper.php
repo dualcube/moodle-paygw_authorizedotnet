8dcdb3edabbdefb494b5c56d6da486d2a83c028a
Sagar Saha <ss2749968@gmail.com>
2025-09-04 15:24:40 +0530

    Update authorizedotnet_helper.php

--- classes/authorizedotnet_helper.php
+++ classes/authorizedotnet_helper.php
@@ -178,8 +178,6 @@ class authorizedotnet_helper {
             return ['success' => false, 'message' => 'No response from Authorize.Net'];
         }
 
-        $response = preg_replace('/^\xEF\xBB\xBF/', '', $response);
-
         $result = json_decode(trim($response), true);
         if (json_last_error() !== JSON_ERROR_NONE) {
             return ['success' => false, 'message' => 'Invalid JSON response from Authorize.Net'];
